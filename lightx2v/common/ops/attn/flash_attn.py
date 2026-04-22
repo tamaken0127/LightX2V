c7d1f6aa6bf151c3c0591176b2b9db75d224589a
tamaken0127 <60533476+tamaken0127@users.noreply.github.com>
2026-04-22 18:29:28 +0900

    Update flash_attn.py

--- lightx2v/common/ops/attn/flash_attn.py
+++ lightx2v/common/ops/attn/flash_attn.py
@@ -17,11 +17,7 @@ except ImportError:
     logger.info("flash_attn_varlen_func_v3 not found, please install flash_attn3 first")
     flash_attn_varlen_func_v3 = None
 
-try:
-    from flash_attn.cute import flash_attn_func as flash_attn_func_v4
-except ImportError:
-    logger.info("flash_attn.cute not found, please install flashattention4 first")
-    flash_attn_func_v4 = None
+flash_attn_func_v4 = None
 
 
 from lightx2v.utils.registry_factory import ATTN_WEIGHT_REGISTER
