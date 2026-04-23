75cb34ce82e71f83fee296b016f39c93b9d4bd20
tamaken0127 <60533476+tamaken0127@users.noreply.github.com>
2026-04-23 20:03:00 +0900

    Update __init__.py

--- lightx2v/common/ops/attn/__init__.py
+++ lightx2v/common/ops/attn/__init__.py
@@ -1,15 +1,17 @@
-from .draft_attn import DraftAttnWeight
 from .flash_attn import FlashAttn2Weight, FlashAttn3Weight, FlashAttn4Weight, SparseFlashAttn4Weight
 from .general_sparse_attn import GeneralSparseAttnWeight
-from .nbhd_attn import NbhdAttnWeight, NbhdAttnWeightFlashInfer
-from .radial_attn import RadialAttnWeight
 from .ring_attn import RingAttnWeight
 from .sage_attn import SageAttn2Weight, SageAttn3Weight, SparseSageAttn2Weight, SparseSageAttn3Weight
 from .sla_attn import SlaAttnWeight
 from .sparge_attn import SpargeAttnWeight
 from .sparse_mask_generator import NbhdMaskGenerator, SlaMaskGenerator, SpargeMaskGenerator, SvgMaskGenerator
 from .sparse_operator import FlashinferOperator, FlexBlockOperator, MagiOperator, SlaTritonOperator, SparseFlashAttentionV4Operator, SparseSageAttentionV2Operator, SparseSageAttentionV3Operator
-from .svg2_attn import Svg2AttnWeight
-from .svg_attn import SvgAttnWeight
 from .torch_sdpa import TorchSDPAWeight
 from .ulysses_attn import Ulysses4090AttnWeight, UlyssesAttnWeight
+
+# 以下は使用時にコメントを外す
+# from .draft_attn import DraftAttnWeight
+# from .nbhd_attn import NbhdAttnWeight, NbhdAttnWeightFlashInfer
+# from .radial_attn import RadialAttnWeight
+# from .svg2_attn import Svg2AttnWeight
+# from .svg_attn import SvgAttnWeight
